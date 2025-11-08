166ce3a1b565d36b1ad462ca400b9ce83669fbf7
Jason <jasonetienne1@gmail.com>
2025-11-08 15:08:17 +0000

    added dep

--- cy-10/eslint.config.js
+++ cy-10/eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import cypress from 'eslint-plugin-cypress'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -24,6 +25,15 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    },
+  },
+  {
+    files: ['cypress/**/*.cy.{js,jsx}'],
+    ...cypress.configs.recommended,
+    languageOptions: {
+      globals: {
+        ...cypress.environments.globals.globals,
+      },
     },
   },
 ])
